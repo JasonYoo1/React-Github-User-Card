6f7851c2c894c7c2983516b90d205a8a252bd1b2
JasonYoo1 <jyoojs@gmail.com>
2019-08-06 14:01:59 -0700

    created fetch with redering information

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -2,25 +2,41 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+class App extends React.Component{
+  constructor(){
+    super();
+    this.state = {
+      users:[]
+    }
+  }
+
+  componentDidMount(){
+    this.fetchInfo()
+  }
+
+  fetchInfo = () => {
+    fetch(`https://api.github.com/users/JasonYoo1`)
+    .then(res=>{
+      return res.json()
+    })
+    .then(info=> this.setState({ users:info }))
+    .catch(err => console.log(err))
+  }
+
+render(){
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Hello World
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
+
+}
+
 }
 
 export default App;
